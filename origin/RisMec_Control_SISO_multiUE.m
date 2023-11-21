020e790a85931f45fb01124c03c8f8f3e7c3d0ac
Victor Croisfelt <victorcroisfelt@gmail.com>
2023-11-21 13:45:28 +0100

    updated with channel_generation.m

--- origin/RisMec_Control_SISO_multiUE.m
+++ origin/RisMec_Control_SISO_multiUE.m
@@ -1,7 +1,9 @@
-clear all; clc; rand('state',0); randn('state',0)
 
-% DATA
-K = 2;                                         % Number of Users
+%% THIS SCRIPT IS VALID FOR SISO AND MULTIPLE UEs
+
+clear all; clc; rand('state',0); randn('state',0)      
+
+% Optimization parameters and data
 N_slot = 3e3;                                  % Number of slots
 Msample = N_slot/2;                            % slot to avoid for the final average
 total_delta = 10e-3;                           % Total slot duration
@@ -11,18 +13,12 @@ delta_signaling = (1 - perc) * total_delta;    % Slot duration dedicated to cont
 alpha = 0.5;                                   % Weight of user and network energy consumption (e.g. 0.5 --> holistic strategy)
 num_iter = 1;                                  % number of iterations for possible sample average
 
-%% Communication parameters
-Pt = 100e-3 * ones(K, 1);                      % Maximum user Tx power
-B = 100e6;                                     % Total bandwidth
-B_u = B / K;                                   % Total bandwidth UL per user
-F = 10^0.5 ;                                   % Noise figure
-N0 = 4e-21 * F;                                % Noise PSD
-p_AP_tx = 250e-3;                              % AP Tx power
-p_AP_on = 2.2;                                 % AP power ON
-
 %% RIS parameters 
-N_RIS = 100;                   % Number of RIS elements
-N_blocks = 100;                % Number of blocks to reduce complexity (e.g. if N_RIS = 100 and N_blocks = 25, 4 elements together are optimized)
+% Number of RIS elements !!! NEEDS TO BE A PERFECT SQUARE FOR CHANNELS GENERATION
+N_RIS = 100;                   % e.g. tried also for 64, 81, 144, 
+% Number of blocks to reduce complexity (e.g. if N_RIS = 100 and N_blocks = 100/4 = 25, 4 elements together are optimized)
+N_blocks = N_RIS/1;            % be very careful in choosing the right divider respect to N_RIS
+                               
 qtz_RIS = 1;                   % Number of bits used to quantize the phase
 if qtz_RIS == 1
     p_bit = .5e-3;             % Power consumption of a RIS element, depending on the number of bits
@@ -31,6 +27,39 @@ elseif qtz_RIS == 2
 elseif qtz_RIS == 3
     p_bit = 1.5e-3;
 end
+
+%% Number of UEs and Channel generation (SISO)
+Nt = 1;                 % Number of TX antennas
+Nr = 1;                 % Number of RX antennas 
+rice_fac = 3;           % Rician factor
+alpha_dir = 3;          % FSPL exponent of the direct link
+f = 2e9;                % Frequency
+
+% due to the particular geometric constraction of channels, be carefull to
+% change the number of UEs without changing the positions in terms of distances!
+K = 4;                                    % Number of Users(1, 2 or 4)
+Dvec = [500, 400, 600, 300];              % TX-RX distance
+dist_ris_vec = [40, 30, 50, 40];          % RIS distance from TX
+lt_vec = [50, 40, 50, 30];                % TX position
+lr = 50;                                  % RX position
+
+overall_channel_user_RIS = zeros(N_RIS, K, N_slot); overall_channel_RIS_AP = zeros(K, N_RIS, N_slot); overall_channel_AP = zeros(K, N_slot);
+for qq = 1:K
+    D = Dvec(qq); dist_ris = dist_ris_vec(qq); lt = lt_vec(qq);
+    [Hdirt, H1t, H2t] = channel_mat_RIS(Nt, Nr, N_RIS, lt, lr, D, N_slot, rice_fac, f, dist_ris, alpha_dir);
+    overall_channel_user_RIS(:, qq, :) = cell2mat(H1t);
+    overall_channel_RIS_AP(qq, :, :) = reshape(cell2mat(H2t), N_RIS, N_slot);
+    overall_channel_AP(qq, :) = cell2mat(Hdirt);
+end
+
+%% Communication parameters
+Pt = 100e-3 * ones(K, 1);                      % Maximum user Tx power
+B = 100e6;                                     % Total bandwidth
+B_u = B / K;                                   % Total bandwidth UL per user
+F = 10^0.5 ;                                   % Noise figure
+N0 = 4e-21 * F;                                % Noise PSD
+p_AP_tx = 250e-3;                              % AP Tx power
+p_AP_on = 2.2;                                 % AP power ON
 
 %% Computation parameters 
 possible_f = (0:0.01:1) * 4.5e9;      % Possible CPU cycle frequencies
@@ -45,38 +74,13 @@ A1 = zeros(K, N_slot);
 for kk = 1:K
     A1(kk,:) = poissrnd(A_avg(kk), 1, N_slot);
 end
-D_avg = 50e-3 * ones(K, 1);                % Delay constraint
+D_avg = 30e-3 * ones(K, 1);                % Delay constraint
 Q_avg = D_avg .* A_avg / total_delta;      % Average queue length corresponding to desired average delay
 
 %%% the vector V1 can be represented also by one value, usually the last one = the one for which the trade-off is optimal 
-%%% the vector is necessary only for the main trade-off
+%%% the vector is necessary only for the Energy/Delay trade-off
 V1 = [1e8; 1e9; 2e9; 3e10];                % Lyapunov trade-off parameter
-% V1 = 3.8e10;
-
-%% Channel generation (SISO)
-Nt = 1;                 % Number of TX antennas
-Nr = 1;                 % Number of RX antennas       
-rice_fac = 3;           % Rician factor    
-D = 500;                % TX-RX distance
-dist_ris = 40;          % RIS distance from TX
-lt = 20;                % TX position 
-lr = 100;               % RX position      
-alpha_dir = 3;          % FSPL exponent of the direct link
-f = 28e9;               % Frequency
-overall_channel_user_RIS_up = zeros(N_RIS, K, N_slot); overall_channel_RIS_AP_up = overall_channel_user_RIS_up; overall_channel_AP_up = zeros(K, N_slot);
-for qq = 1:N_slot
-        [Hdirt, H1t, H2t] = channel_mat_RIS(Nt, Nr, N_RIS, lt, lr, D, K, rice_fac, f, dist_ris, alpha_dir);
-        overall_channel_user_RIS_up(:, :, qq) = cell2mat(H1t);
-        overall_channel_RIS_AP_up(:, :, qq) = cell2mat(H2t')';
-        overall_channel_AP_up(:, qq) = cell2mat(Hdirt);
-end
-
-% % eventual additional blockage pathloss on DIRECT link
-% add_pathloss = 20;                        % Deep fading in dB
-% add_pathloss = 10^(add_pathloss/10);
-% overall_channel_AP_up = overall_channel_AP_up/sqrt(add_pathloss);
-% !!! be carefull to select in case the rigth parameter V1 !!!
-% maybe it should be necessary to change also TX/RX positions and distance
+V1 = 4e11;
 
 %% Structures Inizialization
 energy_tot = zeros(K, N_slot, length(V1)); energy_tot_mec = zeros(N_slot, length(V1)); energy_tot_AP = energy_tot_mec; energy_tot_RIS = energy_tot_mec; 
@@ -91,7 +95,6 @@ for iter = 1:num_iter
      % Loop over trade-off parameter V 
      for vv = 1:length(V1)
         V = V1(vv);       
-       
         % Loop over the Slots
         for nn = 1:N_slot
             % shallow slot counting
@@ -99,21 +102,17 @@ for iter = 1:num_iter
                 nn
             end
             A = A1(:, nn);            
-            
             % Optimization Function
             [power_up, rate_up, freq_MEH, power_mec, current_v, power_RIS] = optimization_RIS(K, B_u,...
                 Q_local(:, nn, vv), Q_MEH(:, nn, vv), Z(:, nn, vv), qtz_RIS, N_RIS, N_blocks,...
-                overall_channel_user_RIS_up(:, :, nn), overall_channel_RIS_AP_up(:, :, nn), overall_channel_AP_up(:, nn),...
+                overall_channel_user_RIS(:, :, nn), overall_channel_RIS_AP(:, :, nn), overall_channel_AP(:, nn),...
                 N0, V, Pt, possible_f, J, delta, p_bit, kappa, alpha);
-            
             % Physical queues update          
             Q_local(:, nn+1, vv) = max(0, Q_local(:,nn,vv) - delta * rate_up) + A;
             Q_MEH(:,nn+1,vv) = max(0, Q_MEH(:,nn,vv) - freq_MEH * delta .* J) + min(Q_local(:, nn, vv), delta * rate_up);
-            
             % Virtual queues update
             Z(:, nn+1, vv) = max(0, Z(:, nn, vv) + Q_local(:, nn+1, vv) + Q_MEH(:, nn+1, vv) - Q_avg);
             Z_tot(:, nn, vv) = Z_tot(:, nn, vv) + Z(:, nn, vv); 
-            
             % Energy Consumption and delay Updates 
             energy_tot(:, nn, vv) = power_up * delta + Pt .* delta_signaling;
             energy_tot_AP(nn, vv) = delta * (p_AP_on * max(power_up>0)) + delta_signaling * (p_AP_on + p_AP_tx);
@@ -136,10 +135,9 @@ end
 %%% parameter to evaluate if the queues are converging = the delay constraint is satisfied
 % = is under the threshold; futhermore, the delay has to increase for greater V1 values
 figure
-loglog(V1, avg_delay,'-s','Linewidth',4,'MarkerSize',12);
+loglog(V1,avg_delay,'-s','Linewidth',4,'MarkerSize',12);
 grid on
 hold on
-
 yline(1e3*D_avg,'-.','alpha',1,'Linewidth',3)
 xlabel('Lyapunov trade-off parameter $V$','Interpreter','Latex')
 ylabel('Average Delay','Interpreter','Latex')
@@ -173,7 +171,7 @@ xlabel('time slots','Interpreter','Latex')
 %% List of Functions %%%%---------------------------
 
 %%% CHANNEL %%%
-function [Hdir, H1, H2] = channel_mat_RIS(Nt, Nr, Nris, lt, lr, D, no_mat, K, f, dist_ris, varargin)
+function [Hdir, H1, H2] = channel_mat_RIS(Nt, Nr, Nris, lt, lr, D, no_mat, Nslot, f, dist_ris, varargin)
 % data
 lambda = 3e8/f;     % Wavelength
 dt = lambda/2;      % TX antenna space
@@ -214,7 +212,7 @@ end
 Hdir_los = exp(-1i*k*d);                             % Direct link, LOS matrix exponents 
 tx_rx_dist = sqrt(D^2+(lt-lr)^2);                    % TX-RX distance   
 FSPL_dir = (lambda/(4*pi))^2/tx_rx_dist^alpha(1);    % Inversion of the FSPL of the direct link 
-Hdir = Rician(Hdir_los,sqrt(FSPL_dir),no_mat,K);     % Direct link channel matrix           
+Hdir = Rician(Hdir_los,sqrt(FSPL_dir),no_mat,Nslot);     % Direct link channel matrix           
 
 % indirect paths (TX-RIS-RX)
 for l1 = 1:Nris                                      % Distance between the RIS elements and the RX antennas                                                            
@@ -236,12 +234,12 @@ FSPLindir = lambda^4/(256*pi^2)*...                  % Inversion of the FSPL of 
 % TX-RIS channel matrix
 H1_los = exp(-1i*k*d1);                             % TX-RIS link, LOS matrix exponents  
 FSPL_1 = sqrt(FSPLindir);                           % FSPL of the indirect link is embedded in the TX-RIS channel matrix 
-H1 = Rician(H1_los,FSPL_1,no_mat,K);
+H1 = Rician(H1_los,FSPL_1,no_mat,Nslot);
 
 % RIS-RX channel matrix
 H2_los = exp(-1i*k*d2);                             % RIS-RX link, LOS matrix exponents 
 FSPL_2 = 1;
-H2 = Rician(H2_los,FSPL_2,no_mat,K);
+H2 = Rician(H2_los,FSPL_2,no_mat,Nslot);
 end
 
 function pos = RISPosition(N1,N2,dist,center)                 % Determine positions of RIS elements
@@ -263,7 +261,7 @@ end
 
 %%% OPTIMIZATION %%%
 function [optimal_power_up, optimal_rate_up, freq_MEH, power_mec, current_v, power_RIS] = optimization_RIS(K, B_u,...
-    Q_local, Q_MEH, Z, qtz_RIS, N_RIS, N_blocks, channel_RIS_up, channel_RIS_AP_up, channel_AP_up,...
+    Q_local, Q_MEH, Z, qtz_RIS, N_RIS, N_blocks, channel_RIS, channel_RIS_AP, channel_AP,...
     N0, V, Pt, possible_f, J, delta, p_bit, kappa, alpha)
 
     % Rate Optimization
@@ -273,23 +271,13 @@ function [optimal_power_up, optimal_rate_up, freq_MEH, power_mec, current_v, pow
     possible_v = [0; exp(1i*(2*m*pi) / (2^(qtz_RIS)))];
 
     current_v = zeros(N_RIS, 1);
-    current_v = Greedy_RIS(current_v, N_blocks, N_RIS, possible_v, K, channel_RIS_up, channel_RIS_AP_up,...
-           channel_AP_up, N0, B_u, V, Q_tilde_up, p_bit, alpha);
-    
-    proba = 0.9;
-    toss_coins = rand(K, 1);
-    config_v = dftmtx(N_RIS);
-
+    current_v = Greedy_RIS(current_v, N_blocks, N_RIS, possible_v, K, channel_RIS, channel_RIS_AP,...
+           channel_AP, N0, B_u, V, Q_tilde_up, p_bit, alpha);
+   
     overall_channel_RIS_up = zeros(K, 1); channel_over_noise_up = zeros(K, 1);
     for kk = 1:K
-        
-        %if toss_coins(kk) < proba
-        overall_channel_RIS_up(kk) = overall_channel_RIS_up(kk) + (channel_RIS_up(:, kk).' * config_v * channel_RIS_AP_up(:, kk));
-        %else
-            %overall_channel_RIS_up(kk) = overall_channel_RIS_up(kk) + (channel_RIS_up(:, kk).' * diag(current_v) * channel_RIS_AP_up(:, kk));
-        %end
-        channel_over_noise_up(kk, 1) = abs(channel_AP_up(kk) + overall_channel_RIS_up(kk)) .^2 / (N0*B_u);
-        
+        overall_channel_RIS_up(kk) = overall_channel_RIS_up(kk) + (channel_RIS_AP(kk, :) * diag(current_v) * channel_RIS(:, kk));
+        channel_over_noise_up(kk, 1) = abs(channel_AP(kk) + overall_channel_RIS_up(kk)) .^2 / (N0*B_u);
         % Uplink 
         rate_max_up(kk) = min(B_u * log2(1 + channel_over_noise_up(kk) .* Pt(kk)), Q_local(kk) / delta);
         p_max(kk) = 1 ./ channel_over_noise_up(kk) .* (exp(rate_max_up(kk) * log(2) / B_u) - 1);
@@ -332,8 +320,8 @@ function [optimal_power_up, optimal_rate_up, freq_MEH, power_mec, current_v, pow
 end
 
 %%% GREEDY RIS OPTIMIZATION FUNCTION %%%
-function current_v = Greedy_RIS(current_v, N_blocks, N_RIS, possible_v, K, channel_RIS_up, channel_RIS_AP_up,...
-    channel_AP_up, N0, B_u, V, Q_tilde_up, p_bit, alpha)
+function current_v = Greedy_RIS(current_v, N_blocks, N_RIS, possible_v, K, channel_RIS, channel_RIS_AP,...
+    channel_AP, N0, B_u, V, Q_tilde_up, p_bit, alpha)
     N_RIS_block = N_RIS / N_blocks;
     for ll = 1:N_blocks
         current_objective_greedy = +Inf;
@@ -342,8 +330,8 @@ function current_v = Greedy_RIS(current_v, N_blocks, N_RIS, possible_v, K, chann
             overall_channel_RIS_up = zeros(K, 1); channel_over_noise_up = zeros(K, 1);
             try_v((ll - 1) * N_RIS_block + 1 : ll * N_RIS_block) = possible_v(vv);
             for kk = 1:K
-                overall_channel_RIS_up(kk) = overall_channel_RIS_up(kk) + (channel_RIS_up(:, kk).' * diag(try_v) * channel_RIS_AP_up(:, kk));
-                channel_over_noise_up(kk) = abs(channel_AP_up(kk) + overall_channel_RIS_up(kk)) .^2 / (N0 * B_u);
+                overall_channel_RIS_up(kk) = overall_channel_RIS_up(kk) + (channel_RIS_AP(kk, :) * diag(try_v) * channel_RIS(:, kk));
+                channel_over_noise_up(kk) = abs(channel_AP(kk) + overall_channel_RIS_up(kk)) .^2 / (N0 * B_u);
             end
 
             objective_greedy = V * p_bit * sum(abs(try_v)) * (1 - alpha) - sum(channel_over_noise_up .* max(Q_tilde_up, 0));
