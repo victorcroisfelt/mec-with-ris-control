a2c357ce7a663b4c9c8770849e5cf4ca3ba41c70
Victor Kreutzfeldt <victorcroisfelt@gmail.com>
2023-11-21 13:32:35 +0100

    Add files via upload

--- RISPosition.m
+++ RISPosition.m
@@ -2,6 +2,6 @@
 function pos = RISPosition(N1,N2,dist,center)                 
     d1 = (0:N1-1)-(N1-1)/2;
     d2 = (0:N2-1)-(N2-1)/2;
-    pos{1} = center(1)+d1*dist;
-    pos{2} = center(2)+d2*dist;
+    pos{1} = center(1 )+ d1 * dist;
+    pos{2} = center(2) + d2 * dist;
 end 
